bb2d7ee4c22d8ef7025fc8662bed541756c6b2a1
cotabas <cotabas@gmail.com>
2022-06-21 22:03:30 -0700

    Hardest one yet, super fun though. used a prime sieve, cool to know but probably useless.. I did learn about streams and have an even deeper understanding of enumerables

--- 3_kyu/prime_stream.rb
+++ 3_kyu/prime_stream.rb
@@ -1,15 +1,28 @@
 class Primes
-  @i = 2
-  @prime = true
-  def self.stream
-    loop do
-    @prime = true
-    @i += 1
+  limit =  16000000
+  key = Array.new(limit, true)
+
+  (2..Math.sqrt(limit).ceil).each do |num|
+    next unless key[num] == true
     
-    for count in (1..Math.sqrt(@i).ceil) do
-     @prime = false if @i % count == 0
-    end
-    return @i if @prime
-    end
+    i = 1
+    rem = num ** 2
+    while rem <= limit do
+      key[rem] = false
+      rem = (num ** 2) + (i * num)
+      i += 1
+    end   
   end
+  
+  key[0], key[1] = false, false
+  @answer = key.each_index.select { |dex| key[dex] == true }
+  
+  def self.stream
+    @answer.to_enum
+  end
+
 end
+
+# 16mil is a lot more than 1..
+# Old man's sieve. I tried the atkins diet but I didn't understand it well enough to optimize
+# It took me way too long to realize the sieve was being call more than once in the sttream method
