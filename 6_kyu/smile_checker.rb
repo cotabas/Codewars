9f4dacf5e6d1a8b7133a08586668e06ebc4e8619
cotabas <cotabas@gmail.com>
2022-11-02 14:31:51 -0700

    better

--- 6_kyu/smile_checker.rb
+++ 6_kyu/smile_checker.rb
@@ -2,6 +2,6 @@ def count_smileys(arr)
   arr.map { |face| 
       [';', ':'].any?(face[0]) &&
       ['-', '~', ')', 'D'].any?(face[1]) &&
-      ([')', 'D'].any?(face[2]) || face[2].nil?)    
+      [')', 'D', nil].any?(face[2])
     }.count(true)
 end
